7b753020660fc8646bff2054dc9e5113114536f3
Md Abdul Mannan <147591955+mannanswe@users.noreply.github.com>
2023-10-25 13:56:45 +0600

    Update mestimenu.js

--- js/mestimenu.js
+++ js/mestimenu.js
@@ -96,36 +96,3 @@ jQuery(document).ready(function () {
   Sfc2.init();
 });
 
-// Profile Page JS Code
-// Profile Page JS Code
-// form open
-const openPassForm = document.getElementById("openPassForm");
-const formContainer = document.getElementById("formContainer");
-const closeButton = document.getElementById("closeButton");
-function toggleForm() {
-  formContainer.classList.toggle("hidden");
-}
-openPassForm.addEventListener("click", toggleForm);
-closeButton.addEventListener("click", toggleForm);
-
-// form open with margin
-
-const openForm = document.getElementById("openForm");
-const openFormContainer = document.getElementById("openFormContainer");
-const openFormClose = document.getElementById("openFormClose");
-
-openForm.addEventListener("click", function () {
-  openFormContainer.classList.toggle("hidden");
-  if (!openFormContainer.classList.contains("hidden")) {
-    document.getElementById("otherDiv").classList.add("with-margin");
-  } else {
-    document.getElementById("otherDiv").classList.remove("with-margin");
-  }
-});
-
-openFormClose.addEventListener("click", function () {
-  openFormContainer.classList.add("hidden");
-  document.getElementById("otherDiv").classList.remove("with-margin");
-});
-// End Profile Page JS Code
-// End Profile Page JS Code
